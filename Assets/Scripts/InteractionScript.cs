d512e9d4e400b1b57649bb2bdf6345d8ddbaf283
Link753 <119621532+Link753@users.noreply.github.com>
2024-02-03 16:12:44 +0000

    Added a check to see if Raycast has value and documented it

--- Assets/Scripts/InteractionScript.cs
+++ Assets/Scripts/InteractionScript.cs
@@ -5,18 +5,22 @@ public class InteractionScript : MonoBehaviour
     RaycastHit hit = new();
     [SerializeField]GameObject InteractableTEXT;
     [SerializeField]GameObject Interactables;
+    bool ifHit;
     // Update is called once per frame
     void Update()
     {
         Debug.DrawRay(transform.position, transform.forward * 3, Color.red);
-        Physics.Raycast(transform.position, transform.forward, out hit, 3);
-        if(hit.transform.gameObject.layer == 3)
+        ifHit = Physics.Raycast(transform.position, transform.forward, out hit, 3);
+        if (ifHit)
         {
-            InteractableTEXT.SetActive(true);
-        }
-        else
-        {
-            InteractableTEXT.SetActive(false);
+            if (hit.transform.gameObject.layer == 3)
+            {
+                InteractableTEXT.SetActive(true);
+            }
+            else
+            {
+                InteractableTEXT.SetActive(false);
+            }
         }
     }
 }
