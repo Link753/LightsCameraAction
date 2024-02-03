e988944d49b6d006d04c4a02d7aa5de0a055c2bb
Link753 <119621532+Link753@users.noreply.github.com>
2024-02-03 16:06:22 +0000

    Imported TMP and visual feedback for interactions

--- Assets/Scripts/InteractionScript.cs
+++ Assets/Scripts/InteractionScript.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class InteractionScript : MonoBehaviour
 {
     RaycastHit hit = new();
-
+    [SerializeField]GameObject InteractableTEXT;
+    [SerializeField]GameObject Interactables;
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward * 5, Color.red);
+        Debug.DrawRay(transform.position, transform.forward * 3, Color.red);
+        Physics.Raycast(transform.position, transform.forward, out hit, 3);
+        if(hit.transform.gameObject.layer == 3)
+        {
+            InteractableTEXT.SetActive(true);
+        }
+        else
+        {
+            InteractableTEXT.SetActive(false);
+        }
     }
 }
