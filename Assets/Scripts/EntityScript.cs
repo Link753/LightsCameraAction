fbb099eb253df6704166494ec5e63a1f0fc040c2
Link753 <119621532+Link753@users.noreply.github.com>
2024-03-25 13:33:58 +0000

    Entity can look at the player and has code to follow the player

---
+++ Assets/Scripts/EntityScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EntityScript : MonoBehaviour
+{
+    int Stage;
+    NavMeshAgent agent;
+    [SerializeField] Transform Player;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        Stage = 0;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        Player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.red);
+        if(Stage == 0)
+        {
+            transform.LookAt(Player);
+        }
+        else if(Stage == 1)
+        {
+            if (Mathf.Abs(Vector3.Distance(transform.position, Player.position)) > 10)
+            {
+                agent.SetDestination(Player.position);
+            }
+            else
+            {
+                agent.SetDestination(transform.position);
+            }
+        }
+        else if(Stage == 2)
+        {
+            agent.SetDestination(Player.position);
+        }
+    }
+}
