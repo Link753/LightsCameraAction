902ad888bf5d65656c10e5161a9613e26b3866b0
Link753 <119621532+Link753@users.noreply.github.com>
2024-03-27 16:07:55 +0000

    prepping level

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class PlayerData
 {
     public int CameraBatteryLevel, takenPictures, flagsSet;
-    public PlayerData(int camBatLevel, int taken, int flagNO)
+    public float[] PlayerPos, PlayerRot;
+    public PlayerData(int camBatLevel, int taken, int flagNO, Transform Player)
     {
+        PlayerPos = new float[3];
+        PlayerRot = new float[3];
+        PlayerPos[0] = Player.position.x;
+        PlayerPos[1] = Player.position.y;
+        PlayerPos[2] = Player.position.z;
+        PlayerRot[0] = Player.rotation.x;
+        PlayerRot[1] = Player.rotation.y;
+        PlayerRot[2] = Player.rotation.z;
         CameraBatteryLevel = camBatLevel;
         takenPictures = taken;
         flagsSet = flagNO;
