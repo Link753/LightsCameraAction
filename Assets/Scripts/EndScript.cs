8553b8509d6d47410dba5cfb68a3a3dcf39a48b9
Link753 <119621532+Link753@users.noreply.github.com>
2024-04-10 16:52:40 +0100

    Integrating End Scene into storyline, after final linking, the game is complete without sounds

--- Assets/Scripts/EndScript.cs
+++ Assets/Scripts/EndScript.cs
@@ -7,6 +7,8 @@ public class EndScript : MonoBehaviour
 {
     [SerializeField] GameObject[] DisableAndAnimate;
     [SerializeField] Animator[] Animators;
+    [SerializeField] GameObject UI;
+    [SerializeField] GameObject[] DisableThese;
     // Start is called before the first frame update
 
     IEnumerator Wait(float waittime)
@@ -18,14 +20,29 @@ public class EndScript : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("Ending");
+        foreach(GameObject g in DisableThese)
+        {
+            g.SetActive(false);
+        }
+        UI.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.None;
         foreach(GameObject g in DisableAndAnimate)
         {
+
             if (g.GetComponent<MonoBehaviour>())
             {
                 g.GetComponent<MonoBehaviour>().enabled = false;
             }
             g.GetComponent<Transform>().localRotation = Quaternion.Euler(Vector3.zero);
+        }
+
+        foreach(Animator A in Animators)
+        {
+            if (!A.enabled)
+            {
+                A.enabled = true;
+            }
         }
 
         Animators[0].SetBool("IsEnding", true);
