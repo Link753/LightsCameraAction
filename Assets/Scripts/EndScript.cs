14aaff37138adbf538974ca9262d5d21625ae130
Link753 <119621532+Link753@users.noreply.github.com>
2024-04-10 14:23:57 +0100

    End Scene Made, Code needs to be added as end scene effects are active for some reason

--- Assets/Scripts/EndScript.cs
+++ Assets/Scripts/EndScript.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndScript : MonoBehaviour
 {
     [SerializeField] GameObject[] DisableAndAnimate;
+    [SerializeField] Animator[] Animators;
     // Start is called before the first frame update
+
+    IEnumerator Wait(float waittime)
+    {
+        yield return new WaitForSeconds(waittime);
+        SceneManager.LoadScene(0);
+        Debug.Log("Waiting");
+    }
+
     void Start()
     {
         Debug.Log("Ending");
         foreach(GameObject g in DisableAndAnimate)
         {
-            g.GetComponent<MonoBehaviour>().enabled = false;
+            if (g.GetComponent<MonoBehaviour>())
+            {
+                g.GetComponent<MonoBehaviour>().enabled = false;
+            }
+            g.GetComponent<Transform>().localRotation = Quaternion.Euler(Vector3.zero);
         }
+
+        Animators[0].SetBool("IsEnding", true);
+        Animators[1].SetBool("IsBeingStabbed", true);
+        Animators[0].SetBool("isStabbed", true);
+        Animators[2].SetBool("IsAfterPlayer", true);
+        StartCoroutine(Wait(5));
     }
 
     // Update is called once per frame
